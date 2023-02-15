7d658ed6ac62320028bd08876eb5b45600d5c53b
Alexandro18 <shura.grashchneko@mail.ru>
2023-02-15 16:44:03 +0300

    work in process

--- task5.py
+++ task5.py
@@ -3,11 +3,11 @@
 if __name__ == '__main__':
 
     l, li = [1, 2, 1, 3, 4, 2, 1, 1, 3, 2], []
-    for i in range(1, len[l] - 1):
-        if l[i - 1] < l[i] and l[i] > l[i + 1]:
-            li.apperand(l[i])
-    count = 1
+    for i in range(len(l)):
+        if l[i-1] < l[i] and l[i] > l[i+1]:
+            li.append(i)
     max = 0
-    for j in range(1, len(l) - 1):
-        for i in range(len(li)):
-            if l[j] == li[i]:
+    for i in range(1, len(li)):
+        if li[i] - li[i-1] > max:
+            max = li[i] - li[i-1]
+    print(max)
